3485a6b5dc3de071db8badaa06888ea7111627e7
lkgusa <140859874+lkgusa@users.noreply.github.com>
2024-08-21 15:42:04 -0400

    test

---
+++ exercises/ex00_hello_world.py
@@ -0,0 +1,10 @@
+# this is called a text editor in a module (file)
+
+"""My first exercise in COMP110! :~}"""
+
+__author__ = "730663471"
+
+
+def greet(name: str) -> str:
+    """A welcoming first function definition."""
+    return "Hello, " + name + "!"
---
+++ first-program.py
@@ -0,0 +1,2 @@
+print("this is a string - howdy!")
+# get help with trailhead, option not appearing in run and debug menu
