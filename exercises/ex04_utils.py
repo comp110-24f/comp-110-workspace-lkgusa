4f9f5fdd6e28c45086848a0380f5586a1b341c5f
lkgusa <140859874+lkgusa@users.noreply.github.com>
2024-10-10 13:51:04 -0400

    Completed exercise 4??

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,57 @@
+"""Utilities of lists"""
+
+__author__ = "730663471"
+
+
+def all(items: list[int], num: int) -> bool:
+    index: int = 0
+    count: int = 0
+    while index < len(items):
+        if items[index] == num:
+            count += 1
+            index += 1
+        else:
+            index += 1
+    if count == len(items):
+        return True
+    else:
+        return False
+
+
+def max(input: list[int]) -> int:
+    if len(input) == 0:
+        raise ValueError("max() arg is an empty List")
+    index: int = 0
+    largest: int = input[
+        0
+    ]  # The largest number is set to the first value of the list by default
+    while index < len(input):
+        if input[index] > largest:
+            # If the value at the current index is greater than largest,
+            # which is set to the first value of the list,
+            # then largest will be updated to be the integer at the current index
+            largest = input[index]
+        index += 1
+        # Even if input[index] is not greater than largest, the index is updated
+        # to the next element in the list
+    return largest
+
+
+def is_equal(list_1: list[int], list_2: list[int]) -> bool:
+    index: int = 0
+    count: int = 0
+    while index < len(list_1) and index < len(list_2):
+        if list_1[index] == list_2[index]:
+            count += 1
+        index += 1
+    if count == len(list_1) and count == len(list_2):
+        return True
+    else:
+        return False
+
+
+def extend(list_1: list[int], list_2: list[int]) -> None:
+    index: int = 0
+    while index < len(list_2):
+        list_1.append(list_2[index])
+        index += 1
