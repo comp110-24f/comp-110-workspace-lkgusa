4300d4ffc10cc0b78218d31f412437915b22c6ba
lkgusa <140859874+lkgusa@users.noreply.github.com>
2024-10-16 09:46:03 -0400

    cq07 complete

--- CQs/cq07/find_max.py
+++ CQs/cq07/find_max.py
@@ -2,10 +2,28 @@ __author__ = "730663471"
 
 
 def find_and_remove_max(input: list[int]) -> int:
-    max_num: int = max(input)
+    max_num: int = 0
+    index = 0
+
     if input == []:
         return -1
-    for elem in input:
-        if elem == max_num:
-            input.pop(elem)
+
+    while index < len(input):
+        # Checks if each list value is larger than last,
+        # should find the max number in the list
+        if input[index] > max_num:
+            max_num = input[index]
+            index += 1
+        else:
+            index += 1
+
+    index = 0
+    # Resets index number because it was at the last value before
+    while index < len(input):
+        if input[index] == max_num:
+            input.pop(index)  # .pop(input[index]) did not work because
+            # pop only understands INDEX values not the values of the list
+        else:
+            index += 1
+
     return max_num
--- CQs/cq07/max_test.py
+++ CQs/cq07/max_test.py
@@ -1,1 +1,19 @@
+from CQs.cq07.find_max import find_and_remove_max
+
 __author__ = "730663471"
+
+
+def test_find_and_remove_max_returns_correct() -> None:
+    sequence: list[int] = [1, 2, 3, 4, 5]
+    assert find_and_remove_max(sequence) == 5
+
+
+def test_find_and_remove_max_mutates_list() -> None:
+    sequence1: list[int] = [1, 7, 2, 8, 6, 8]
+    find_and_remove_max(sequence1)
+    assert sequence1 == [1, 7, 2, 6]
+
+
+def test_find_and_remove_max_edge_case() -> None:
+    sequence2: list[int] = []
+    assert find_and_remove_max(sequence2) == -1
